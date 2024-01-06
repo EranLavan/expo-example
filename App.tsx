2e93abb147054ac36ec63abd6ddf27380aff9ed7
Grigorii Belov <belovgrigory139@gmail.com>
2024-01-06 23:38:00 +0300

    modified:   App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello Menkalinan</Text>
+      <Text>Let's type some code!</Text>
       <StatusBar style="auto" />
     </View>
   );
